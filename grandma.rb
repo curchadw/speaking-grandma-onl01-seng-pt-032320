fc20a6be196f96d772c28ae0b5ecc922bbe2ac05
Curtis Chadwell <laurencechadwell@gmail.com>
2020-03-24 02:36:58 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,7 +14,7 @@
 
 def speak_to_grandma(string)
   if string != string.upcase
-    return ""
+    return "HUH?! SPEAK UP, SONNY!"
   elsif string == string.upcase
     return "NO, NOT SINCE 1938!"
   elsif string ==  "I LOVE YOU GRANDMA!"
